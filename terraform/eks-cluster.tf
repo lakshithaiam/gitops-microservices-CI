22f62170766c8c9f57cab64eb96f7913c1070687
lakshithaiam <lakshithaiam@gmail.com>
2024-10-12 23:08:42 +0530

    update main

---
+++ terraform/eks-cluster.tf
@@ -0,0 +1,28 @@
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "20.24.3"
+
+  cluster_endpoint_public_access = true
+  cluster_name  = "myapp-eks-cluster"  
+  cluster_version = "1.31"
+
+  subnet_ids = module.myapp-vpc.private_subnets
+  vpc_id     = module.myapp-vpc.vpc_id
+
+  enable_cluster_creator_admin_permissions = true
+
+  tags = {
+    environment = "development"
+    application = "myapp"
+  }
+
+  eks_managed_node_groups = {
+    dev = {
+      min_size     = 1
+      max_size     = 3
+      desired_size = 2
+
+      instance_types = ["t2.small"]
+    }
+  }
+}
